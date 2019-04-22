d0d3b51b575d23e1c681a987c5e6cc84061addbe
Thalia Sirathanapornpat <thalia@vlan403-105.subnet-242.amherst.edu>
2019-04-22 19:31:05 -0400

    cleaned code + added comments in Snake.java

--- Snake.java
+++ Snake.java
@@ -1,8 +1,14 @@
+// imports
 import java.awt.Color;
 import java.util.ArrayList;
 import java.awt.Graphics;
 import java.lang.*;
 
+ //===========================================================================
+ // ==========================================================================
+
+// Start of class Snake
+// Contains rules of snake movement and evolution
 
 public class Snake implements Colorable {
   public int inedibleCount = 0;
@@ -11,7 +17,12 @@ public class Snake implements Colorable {
   public static Color color = Color.BLACK;
   public int player;
   public Arena arena;
-  public int velocityComponent = 200; // for debugging let's make it slower for now
+  public int velocityComponent = 200;
+
+
+// constructor for class snake =========================================
+// Takes in an Arena instance and a player number
+// Sets position according to player number and creates the snake's body
 
   public Snake(int player, Arena arena) {
     this.player = player;
@@ -39,14 +50,22 @@ public class Snake implements Colorable {
   } // end of Snake constructor
 
 
-
+// start of method drawSnake ==============================================
+// called in class Arena
   public void drawSnake(Graphics g){
     g.setColor(color);
     g.fillRect((int)head.position.x, (int)head.position.y, head.width, head.height);
     for (Segment s: body){
       g.fillRect((int)s.position.x, (int)s.position.y, s.width, s.height);
      }
-  } // end of drawSnake
+  } // end of drawSnake  =====================================================
+
+
+
+// start of method move =====================================================
+// controls how the snake moves on the screen
+// the head moves according to physics, but the rest of the body moves by moving
+// the last segment to replace the old position of the head
 
   public void move(Pair velocity, double time) {
     Pair oldHeadPosition = head.position;
@@ -55,9 +74,13 @@ public class Snake implements Colorable {
     body.remove(x);
     body.add(0, new Segment(oldHeadPosition, velocity));
 
-  }
+  } // end of move  ============================================================
 
-  // We're only moving the head, then the last segment in the body to replace the old position of the head
+
+  // start of method update  =====================================================
+  // Calls on method move, and checks if the snake is eating itself, its friend,
+  // or running into the wall. If the snake eats an item, it calls on evolve()
+  // to change the snake accordingly
   public void update(Pair velocity, double time){
     this.move(velocity, time);
 
@@ -78,16 +101,23 @@ public class Snake implements Colorable {
     if (this.inedibleCount == 5){
       HISS.state = State.gameOver;
     }
-   } // end of update
+  } // end of update  ========================================================
 
+
+// start of method eatSelf  =====================================================
+// checks if the snake is eating itslef
    public boolean eatSelf(){
      for (Segment s: this.body){
        if (head.position.equalsTo(s.position))
         return true;
      }
      return false;
-   } // end of eatSelf
+   } // end of eatSelf  ========================================================
 
+
+
+// start of method eatFriend  ==================================================
+// checks if the snake is eating its friend
    public boolean eatFriend(){
      Snake friend;
      if (player == 1) {
@@ -102,8 +132,11 @@ public class Snake implements Colorable {
         return true;
      }
      return false;
-   } // end of eatFriend
+   } // end of eatFriend  =====================================================
 
+
+// start of method hitWall  =====================================================
+// checks if the snake is running into the wall
    public boolean hitWall(){
      double x = this.head.position.x;
      double y = this.head.position.y;
@@ -115,9 +148,14 @@ public class Snake implements Colorable {
        hitWall = true;
      }
      return hitWall;
-   } // end of hitWall
+   } // end of hitWall  ========================================================
 
 
+
+
+// start of method changeVelocity  =============================================
+// deals with changing the velocity of the snake based on the character input from
+// the keyboard
   public void changeVelocity(char c) {
     if (isMovingUp() || isMovingDown()) {
       if (c == 'a' || c == 'j') {
@@ -135,9 +173,12 @@ public class Snake implements Colorable {
         head.velocity = new Pair(0, velocityComponent);
       }
     }
-  } // end of changeVelocity
+  } // end of changeVelocity  ==================================================
 
 
+
+// start of method eatenItem  ==================================================
+// returns an item if it is eaten, null if not
   public Item eatenItem(){
     for (Item i: arena.items){
       if(head.position.inRange(i.position, 10)){
@@ -145,25 +186,25 @@ public class Snake implements Colorable {
       }
     }
     return null;
-  } // end of hasEatenItem
+  } // end of hasEatenItem  ====================================================
 
 
-
+//start of method evolve  =====================================================
+// calls on eatenItem() and increases length of snake and score if edible item is eaten
+// increases inedibleCount when inedible item is eaten
   public void evolve(Item item, Pair velocity){
     if(item.edible){
       arena.score++;
-      for (int i=0; i<1; ++i) {
-        Segment s = new Segment(body.get(i).position, velocity);
-        body.add(s);
-      }
+      Segment s = new Segment(body.get(i).position, velocity);
+      body.add(s);
     }
     else{
       inedibleCount++;
     }
-  } // end of evolve
+  } // end of evolve  =========================================================
 
 
-
+// The following methods check the direction that the snake is moving in ======
   public boolean isMovingUp() {
     Pair upVelocity = new Pair(0, -velocityComponent);
     if (head.velocity.equalsTo(upVelocity)){
@@ -196,6 +237,11 @@ public class Snake implements Colorable {
     return false;
   }
 
+// end of methods checking direction of snake movement ======================
+
+// start of method changeColor  ================================================
+// overrides the abstract method from Colorable interface
+// changes color of the snake based on the score
   @Override
     public void changeColor() {
       if (arena.score > 50 && arena.score <= 100) {
@@ -204,13 +250,16 @@ public class Snake implements Colorable {
       if (arena.score > 100) {
         color = Color.BLUE;
       }
-    }
-
-
+    } // end of changeColor  ===================================================
 
 
 } // end of class Snake
 
+ //============================================================================
+ //============================================================================
+
+// start of class Segment
+// the body of the snake is an ArrayList of Segments
 class Segment {
   public Pair position;
   public Pair velocity;
@@ -222,3 +271,6 @@ class Segment {
     this.velocity = velocity;
   }
 }
+// end of class Segment
+//  ==========================================================================
+// ===============================================================================
