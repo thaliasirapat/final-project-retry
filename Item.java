0548fcb94897b43a26b2ae6aed22b606df11458b
Thalia Sirathanapornpat <thalia@vlan403-105.subnet-242.amherst.edu>
2019-04-22 19:35:25 -0400

    cleaned code + added comments to Item.java

--- Item.java
+++ Item.java
@@ -1,21 +1,29 @@
+// imports
 import java.awt.Image;
 import java.util.Random;
 import java.awt.Color;
 import java.awt.Graphics;
 
+//start of class Item
+// defines items and methods for drawing and erasing items
 public class Item {
 
   public Pair position;
   public final int width = 10;
   public final int height = 10;
   public boolean edible;
+  private Random rand = new Random();
 
+  // constructor for Item ====================================================
+  // takes in a boolean value for edible/inedible and randomizes position of item
   public Item(boolean edible) {
-    Random rand = new Random();
     this.position = new Pair((double)rand.nextInt(Arena.width - width), (double)rand.nextInt(Arena.height - height));
     this.edible = edible;
-  }
+  } // end of constructor  ====================================================
 
+
+// start of method drawItem
+// draws items to the screen
   public void drawItem(Graphics g) {
     if (edible == true) {
       g.setColor(Color.RED); //red for apple
@@ -24,10 +32,11 @@ public class Item {
       g.setColor(Color.GRAY); //gray for rocks
     }
     g.fillRect((int)position.x, (int)position.y, this.width, this.height);
-  }
+  } // end of drawItem
 
+// start of eraseItem
+// called when an item is eaten; moves the eaten item to another random position on the screen
   public void eraseItem() {
-    Random rand = new Random();
     this.position = new Pair((double)rand.nextInt(Arena.width - width), (double)rand.nextInt(Arena.height - height));
   }
-}
+} // end of eraseItem
